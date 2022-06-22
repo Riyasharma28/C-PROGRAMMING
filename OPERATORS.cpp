1413808895b5d25f368c8f5afaefffb661cdfcba
Riya Sharma <107835966+Riyasharma28@users.noreply.github.com>
2022-06-22 22:26:47 +0530

    Add files via upload

---
+++ OPERATORS.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	a=20,b=10;  //assignment operator
+
+    
+    //ARITHMETIC OPERATORS:
+    
+    cout<<"Addition: "<<a+b<<endl;
+    cout<<"Subtraction: "<<a-b<<endl;
+    cout<<"Multiplication: "<<a*b<<endl;
+    cout<<"Division: "<<a/b<<endl;
+    cout<<"Modulus: "<<a%b<<endl;
+	
+   //INCREMENT OPERATOR:
+    a++;
+	cout<<"Value of a "<<a<<endl;
+	//DECREMENT OPERATOR:
+	a--;
+	cout<<"Value of a "<<a<<endl;
+	return 0;	
+	
+}
---
+++ VARIABLES.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x,y;   // integer declaration
+	x=2;      //assigning value to x
+	y=x+5;
+	
+	char ch ='a';   //character declaration
+	
+	string name = "Riya Sharma";   //string declaration
+	
+	cout<<"Name of the student is :"<<name<<endl;
+	cout<<"The value of character  is "<<ch<<endl;
+	cout<<"The value of y is "<<y<<endl;
+}
