1cae092a7e50168ad574fa2284d197f0455df3be
Riya Sharma <107835966+Riyasharma28@users.noreply.github.com>
2022-06-26 11:40:32 +0530

    Add files via upload

---
+++ CONDITION(if-else-if).cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout<<"Enter the value of a"<<endl;
+	cin>>a;
+	if(a>0)
+	{
+		cout<<"Positive"<<endl;
+		
+	}
+	else if(a<0)
+	{
+		cout<<"Negative"<<endl;
+	}
+	else
+	{
+		cout<<"Zero"<<endl;
+	}
+	
+return 0;
+}
