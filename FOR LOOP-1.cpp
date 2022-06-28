2733cb80ff237796f42fa79bd5f98232de6c3009
Riya Sharma <107835966+Riyasharma28@users.noreply.github.com>
2022-06-29 00:52:27 +0530

    Add files via upload

---
+++ FOR LOOP-1.cpp
@@ -0,0 +1,11 @@
+//WAP TO PRINT "PROGRAMMER" 5 TIMES:
+#include<iostream>
+using namespace std;
+int main()
+{
+	for(int i=0;i<=5;i++)
+	{
+		cout<<"Programmer"<<endl;
+	}
+	return 0;
+}
---
+++ FOR LOOP-2(Printing tables).cpp
@@ -0,0 +1,11 @@
+//WAP TO PRINT THE TABLE OF 5:
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x=10;
+	for(int i=1;i<=10;i++)
+	{
+		cout<<"5 x"<<" = "<<(x*i)<<endl;
+	}
+}
