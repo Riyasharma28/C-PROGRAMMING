88a66a9751275eca7a2f18d981b23274de6a596d
Riya Sharma <107835966+Riyasharma28@users.noreply.github.com>
2022-07-01 01:29:22 +0530

    Add files via upload

---
+++ DO WHILE LOOP.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char mychar;
+	do{
+		cout<<"I am a programmer"<<endl<<"Enter my character"<<endl;
+		cin>>mychar;
+	}
+	while(mychar=='x');
+	
+	
+	return 0;
+}
